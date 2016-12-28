ac197148c4065d8b5e2cf344770628af3f07b4ea
Caio Portugal <caoportugal92@gmail.com>
2016-12-28 00:34:03 -0200

    Adicionando a classe de negociação

---
+++ client/js/index.js
@@ -0,0 +1,30 @@
+var campos = 
+[
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor'),
+];
+
+var tableBody = document.querySelector('table tbody');
+document.querySelector('.form').addEventListener('submit',function(event){
+    event.preventDefault();
+    var tableRow = document.createElement('tr');
+    campos.forEach(function(campo){
+        var tableData = document.createElement('td');
+        tableData.textContent = campo.value;
+        tableRow.appendChild(tableData);
+    });
+    var tableDataVolume = document.createElement('td');
+    var volume = campos[1].value * campos[2].value;
+    tableDataVolume.textContent = volume;
+    tableRow.appendChild(tableDataVolume);
+    tableBody.appendChild(tableRow);
+    limparFormulario();
+});
+
+var limparFormulario = function(){
+    campos[0].value = '';
+    campos[1].value = 0;
+    campos[2].value = 0;
+    document.querySelector('#data').focus();
+}
